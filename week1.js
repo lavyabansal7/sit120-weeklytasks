c7cca364a711d333c8c5d10bc4acd355d44c4a1c
Lavya <lavyabansal16@gmail.com>
2022-07-31 19:01:07 +1000

    Week1 Tasks

---
+++ week1.js
@@ -0,0 +1,10 @@
+let vue = new Vue({
+    el: '#app',
+    data: {
+        todos: [
+            {title: 'Groceries', checked: false},
+            {title: 'Assignments', checked: true},
+            {title: 'View Netflix', checked: true},
+        ],
+    },
+});
