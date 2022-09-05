e8a88f15b30c4a9a246eb0a004b7fdf4cf9bf24b
Lavya Bansal <lavyabansal16@gmail.com>
2022-09-05 11:22:46 +1000

    week3 tasks

---
+++ week_3/week3.js
@@ -0,0 +1,50 @@
+//TASK 1
+//1
+let text = "Hello User";
+console.log(text[1]);
+
+//2
+let Print_Message = "Lavya";
+console.log(Print_Message);
+
+//3
+let sentence = "Hi," + "" + "User";
+sentence = "Hello ".concat("", "User");
+console.log(sentence); 
+
+//4
+let p= 18;
+let q= 14;
+let r= 12;
+sum = p+q+r
+console.log(sum);
+
+//5
+let NAME = "User";
+console.log(typeof NAME);
+
+console.clear()
+
+//TASK 2
+//1 array
+let names = ['Amit','Lavya','Butcher','Hughie'];
+console.log(names);
+
+//2 push
+console.log(names.push('Olivia'));
+console.log(names);
+
+//3 pop
+console.log(names.pop());
+console.log(names);
+
+//4 sort
+let numbers = [232,234,345,789,0];
+console.log(numbers.sort());
+
+//5 accessing
+console.log(`My idol is ${names[2]}`) ;
+
+
+
+
