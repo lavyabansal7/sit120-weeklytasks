a046cd369ca0b991cafd1063c7b125eed5a7d024
Lavya Bansal <lavyabansal16@gmail.com>
2022-09-09 13:18:44 +1000

    week 6 credit task

---
+++ week_6/week6t3.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            selected: "",
+            dynamicValues: [1, 2, 3],
+            dynamicSelected: "",
+
+        }
+    },
+})
